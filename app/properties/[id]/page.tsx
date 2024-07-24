39a953de33e6e5fdfbfd567293554a29969cdee6
Xarasho <alex.olmedo.rojas@gmail.com>
2024-07-24 01:40:34 -0600

    Made PropertyPage a client side component

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+'use client'
 
 const PropertyPage = () => {
   return (
@@ -7,3 +7,22 @@ const PropertyPage = () => {
 }
 
 export default PropertyPage
+
+// import { useParams, usePathname, useRouter, useSearchParams } from 'next/navigation'
+
+// const PropertyPage = () => {
+
+//   const router = useRouter();
+//   const { id } = useParams();
+//   const searchParams = useSearchParams();
+//   const name = searchParams.get('name');
+//   const pathName = usePathname();
+
+//   return (
+//     <div>
+//       <button onClick={() => router.push('/')} className='bg-blue-500 p-2'>
+//         Go Home {id} {name} {pathName}
+//       </button>
+//     </div>
+//   )
+// }
